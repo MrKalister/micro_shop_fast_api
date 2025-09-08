e034ff9e3527bba38642d05f5913a1755d3bca87
Novikov Maxim <maxon.nowik@yandex.ru>
2025-09-09 00:43:55 +0200

    feat: add router

--- core/config.py
+++ core/config.py
@@ -8,6 +8,7 @@ BASE_DIR = Path(__file__).parent.parent
 
 
 class Settings(BaseSettings):
+    api_v1_prefix: str = "/api/v1"
     file_path: str = os.path.join(BASE_DIR, "sqlite_db")
     db_url: str = f"sqlite+aiosqlite:///{file_path}"
     db_echo: bool = True  # TODO: debug only
--- main.py
+++ main.py
@@ -1,35 +1,29 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
+from core.config import settings
 from core.models import Base, db_helper
-from items_views import router as items_router
-from api_v1.users.views import router as users_router
+from api_v1 import router as router_v1
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    # This code is run before app is launched
+    # create transaction
     async with db_helper.engine.begin() as conn:
+        # Create all DB tables based on models
         await conn.run_sync(Base.metadata.create_all)
         # await conn.run_sync(Base.metadata.drop_all)
+    # This code is run after app is finished
     yield
 
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(items_router)
-app.include_router(users_router)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
 
 @app.get("/")
 def hello_index():
     return {
         "message": "Hello index!",
-    }
-
-
-@app.get("/calc/add/")
-def add(a: int, b: int):
-    return {
-        "a": a,
-        "b": b,
-        "result": a + b,
     }
