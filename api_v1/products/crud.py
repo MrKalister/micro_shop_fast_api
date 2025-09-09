048f77dc53a7b4f66a82b487fcc03b01a888c3a2
Novikov Maxim <maxon.nowik@yandex.ru>
2025-09-10 00:19:19 +0200

    feat: add update, delete operations

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -4,7 +4,7 @@ from sqlalchemy import select
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api_v1.products.schemas import ProductCreate
+from api_v1.products.schemas import ProductCreate, ProductUpdate, ProductUpdatePartial
 from core.models import Product
 
 
@@ -27,3 +27,24 @@ async def create_product(session: AsyncSession, product_in: ProductCreate) -> Pr
     await session.commit()
     # await session.refresh(product)
     return product
+
+
+async def update_product(
+    session: AsyncSession,
+    product: Product,
+    product_update: ProductUpdate,
+    partial: bool = False,
+) -> Product:
+    for name, value in product_update.model_dump(exclude_unset=partial).items():
+        setattr(product, name, value)
+    await session.commit()
+    # await session.refresh(product)
+    return product
+
+
+async def delete_product(
+    session: AsyncSession,
+    product: Product,
+):
+    await session.delete(product)
+    await session.commit()
