6d87d80564a6252a595b4687702bd9c4c59120cf
Novikov Maxim <maxon.nowik@yandex.ru>
2025-09-25 11:49:36 +0200

    feat: remove connect to db on app start

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-from core.config import settings
-from core.models import Base, db_helper
 from api_v1 import router as router_v1
+from core.config import settings
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # This code is run before app is launched
-    # create transaction
-    async with db_helper.engine.begin() as conn:
-        # Create all DB tables based on models
-        await conn.run_sync(Base.metadata.create_all)
-        # await conn.run_sync(Base.metadata.drop_all)
-    # This code is run after app is finished
     yield
 
 
