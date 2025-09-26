14ca11c1fc220cf78eff751ab0b65e028e77fc7a
Novikov Maxim <maxon.nowik@yandex.ru>
2025-09-26 02:03:07 +0200

    feat: create users table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,6 +1,8 @@
-__all__ = ("Base", "Product", "DataBaseHelper", "db_helper")
+__all__ = ("Base", "Product", "DataBaseHelper", "db_helper", "User")
 
 
 from .base import Base
 from .db_helpers import DataBaseHelper, db_helper
 from .product import Product
+
+from .user import User
