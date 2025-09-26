a17c9899fb4784cfcfd6ade5eb1afb014c469227
Novikov Maxim <maxon.nowik@yandex.ru>
2025-09-26 02:26:55 +0200

    feat: create relationships between user and post

--- core/models/post.py
+++ core/models/post.py
@@ -1,7 +1,12 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from . import Base
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Post(Base):
@@ -9,3 +14,4 @@ class Post(Base):
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
     # Using 'int' in typehints we set constraint "nullable=False"
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="posts")
