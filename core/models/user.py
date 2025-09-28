31151311a9fc932164a820c00f754607498aeb39
Novikov Maxim <maxon.nowik@yandex.ru>
2025-09-29 00:48:17 +0200

    feat: add str to user

--- core/models/user.py
+++ core/models/user.py
@@ -14,3 +14,8 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
     posts: Mapped[list["Post"]] = relationship(back_populates="user")
     profile: Mapped["Profile"] = relationship(back_populates="user")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, username={self.username!r})"
+
+    __repr__ = __str__
